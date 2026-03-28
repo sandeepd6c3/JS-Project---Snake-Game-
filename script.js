4829a80322827b94f85147dbb7835b56337b4d49
[Sandeep] <sandeep977244@gmail.com>
2026-03-29 00:47:29 +0530

    Add modal for game start and game over with corresponding styles

--- script.js
+++ script.js
@@ -3,6 +3,23 @@
 // Select the game board element from the DOM
 let Board=document.querySelector(".Board");
 
+const startbutton=document.querySelector(".btn-start")
+const modal=document.querySelector(".modal");
+
+const startgamemodal=document.querySelector(".start-game");
+const gameovermodal=document.querySelector(".game-over");
+const restartbutton=document.querySelector(".btn-restart");
+
+
+const highscoreElement=document.querySelector("#high-score");
+const scoreElement=document.querySelector("#score");
+const timeElement=document.querySelector("#time");
+
+
+let highscore=0; 
+let score=0;
+let time=`00-00`;
+
 // Define block dimensions (height and width in pixels)
  const blockheight=40;
     const blockwidth=40;
@@ -16,7 +33,7 @@ const blocks =[];
     const rows=Math.floor(Board.clientHeight/blockheight);
 
 // Initial snake position as an array of objects with x (row) and y (column) coordinates
-    const snake=[{x:5,y:5}]
+   let  snake=[{x:1,y:3}]
 
 // Initial direction of the snake movement
 let direction='right';
@@ -60,18 +77,26 @@ head={x:snake[0].x-1,y:snake[0].y}
     }
 
     if(head.x<0 || head.x>=rows ||head.y<0 || head.y>=cols){
-      alert("Game Over ")
+      
       clearInterval(setIntervalId)
+      
+      modal.style.display="flex"
+
+      startgamemodal.style.display="none";
+      gameovermodal.style.display="flex";
+      return;
     }
 
     if(head.x==food.x && head.y==food.y){
         blocks[`${food.x}-${food.y}`].classList.remove("food");
         food ={
-  x:Math.floor(Math.random()*rows ),
-  y:Math.floor(Math.random()*cols),
+        x:Math.floor(Math.random()*rows ),
+           y:Math.floor(Math.random()*cols),
 }
  blocks[`${food.x}-${food.y}`].classList.add("food");
  snake.unshift(head);
+ score+=10;
+ scoreElement.innerText=score;
     }
 
 // Remove 'fill' class from current snake positions before moving
@@ -81,6 +106,9 @@ head={x:snake[0].x-1,y:snake[0].y}
 
 // Add new head to the front of the snake
     snake.unshift(head);
+
+    
+   
 // Remove the tail to maintain snake length
     snake.pop();
 
@@ -94,12 +122,44 @@ head={x:snake[0].x-1,y:snake[0].y}
       }
 
 // Game loop that runs every 500ms to move the snake
-   setIntervalId=setInterval(()=>{
+//    setIntervalId=setInterval(()=>{
    
-// Render the updated snake
-render();
-   },200)     
+// // Render the updated snake
+// render();
+//    },200)     
 
+startbutton.addEventListener("click",()=>{
+  modal.style.display="none"
+    setIntervalId=setInterval(() => {
+      render();
+
+    }, 500);
+   })
+
+
+   restartbutton.addEventListener("click",restartgame)
+
+   function restartgame(){
+clearInterval(setIntervalId);
+
+     modal.style.display="none";
+ 
+blocks[`${food.x}-${food.y}`].classList.remove("food");
+
+snake.forEach(segment=>{
+        console.log(segment);
+        blocks[`${segment.x}-${segment.y}`].classList.remove('fill');
+        
+        
+      })
+
+     snake=[{x:1,y:3}]
+      direction='right';
+
+     food ={ x:Math.floor(Math.random()*rows ), y:Math.floor(Math.random()*cols),}
+
+     setIntervalId=setInterval(() => {render()}, 500);
+   }
 
    addEventListener("keydown",(event)=>{
    if(event.key=="ArrowUp"){
@@ -115,3 +175,5 @@ render();
    }
     
    })
+
+    
